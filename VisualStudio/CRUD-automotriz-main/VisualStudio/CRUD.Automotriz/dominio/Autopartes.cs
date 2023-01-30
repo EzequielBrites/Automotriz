9a9544090999600ab5721bf381a831975c06f119
Ezequiel Brites <ezequielbrites0@gmail.com>
2023-01-30 10:12:49 -0300

    avances

---
+++ VisualStudio/CRUD-automotriz-main/VisualStudio/CRUD.Automotriz/dominio/Autopartes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Automotriz.dominio
+{
+    internal class Autopartes
+    {
+        private int id;
+        private string nombre;
+        private string modelo;
+        private int idcolor;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Idcolor { get => idcolor; set => idcolor = value; }
+    }
+}
---
+++ VisualStudio/CRUD-automotriz-main/VisualStudio/CRUD.Automotriz/dominio/Vehiculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Automotriz.dominio
+{
+    internal class Vehiculos
+    {
+        private string marca;
+        private string modelo;
+        private int añofab;
+        private string descripcion;
+
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Añofab { get => añofab; set => añofab = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        public override string ToString()
+        {
+            return "Marca:" + marca + "Modelo:" + modelo + "Descripcion:" + descripcion;
+        }
+    }
+}
